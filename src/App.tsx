49a5a28c830e1aaf5081660ebde5ddac3bc05704
HenrikCheng <48348033+HenrikCheng@users.noreply.github.com>
2022-10-12 10:25:37 +0300

    startup-router-henrik

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { HashRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { Home, Foo, Bar } from "../src/Pages";
+import Login from "./Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/foo">Foo</Link>
+          <Link to="/bar">Bar</Link>
+          <Link to="/Login">Login</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/foo" element={<Foo />} />
+          <Route path="/bar" element={<Bar />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
